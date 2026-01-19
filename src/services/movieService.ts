ad0d1c2c4032081c1e2684c7c4ac47ff85b8eddc
Denys Hedenko <unstoppa6le@gmail.com>
2026-01-19 09:45:50 +0100

    first init

---
+++ src/services/movieService.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import type { Movie } from '../types/movie';
+
+
+interface getFetchMoviesResponse {
+    results: Movie[];
+}
+
+
+export default function fetchMovies() {
+    
+}
